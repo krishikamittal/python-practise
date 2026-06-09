011ad0c3050dd749bea0db1e7738a62b73596c88
Krishika Mittal <krishikamittal15@gmail.com>
2026-06-10 00:41:39 +0530

    Create basic_string_pattern.py

---
+++ basic_string_pattern.py
@@ -0,0 +1,8 @@
+#Program: Demonstration of string pattern  using Python Operations
+#Topic:Binary File, Python Operations
+
+STR=input('enter word :')
+RSTR=''
+for ch in (STR):
+    RSTR=RSTR+ch
+    print(RSTR)
